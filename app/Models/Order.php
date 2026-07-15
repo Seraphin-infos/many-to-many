29a04d32e7a052a8999214f8a3d9dbd982038b93
Seraphin <seraphin.infos@gmail.com>
2026-07-15 09:13:28 +0300

    .

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,16 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Products;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Order extends Model
 {
+    use HasUuids;
     protected $fillable = ['order_number','order_date','total_amount','status'];
 
     public function produit() {
-        return $this->belongsToMany(Products::class);
+        return $this->belongsToMany(Products::class)->withPivot('quantite','prix')->withTimestamps();
     }
 
-    public function total() {
-        $this->quantity * $this->price;
+    public function totalPrice() {
+        $this->quantite * $this->prix;
     }
 }
