9c07cdf9ef67a077e87b555c5eace04a0cb12a63
Seraphin <seraphin.infos@gmail.com>
2026-07-15 09:42:19 +0300

    update productController

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    //
+    public function update(Request $request, Products $products) {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string|max:255',
+            'price' => 'required|decimal:1,20',
+        ]);
+
+        $products->update($validated);
+    }
 }
