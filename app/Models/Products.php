b8044554e8bf52e3df15bfddfd48aeca8f4404da
Fifaliana Andriana <andrianjafyfifaliana@gmail.com>
2026-07-15 09:16:57 +0300

    config

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Products extends Model
 {
     //CRUD DES PRODUITS, AVEC ROUTE
+    public function products(){
+        return $this-> belongsToMany(Products::class, 'products_order');
+        // return $this-> withPivot('enrolled_at');
+        // return $this-> withoutTimestamps();
+    }
 }
