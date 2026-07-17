265e4613df6b909e24770a25c7e61d1026562b07
Muriellah204 <muriellahprecyllah@gmail.com>
2026-07-17 13:21:06 +0300

    correctionroute

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('products', 'App\Http\Controllers\ProductsController');
-
-Route::get('/products/edit', [ProductsController::class, 'edit'])->name('products.edit');
